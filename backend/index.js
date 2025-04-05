70a35faf7085535c9885aa2a0770264ac04d3a73
Thanh nguyen <jonhsonle1122@gmail.com>
2025-04-05 10:45:08 +0700

    complete project

--- backend/index.js
+++ backend/index.js
@@ -3,22 +3,24 @@ import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 import path from "path";
+
 import { connectDB } from "./db/connectDB.js";
+
 import authRoutes from "./routes/auth.route.js";
-const app = express();
 
 dotenv.config();
+
+const app = express();
 const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
-app.use(express.json());
-app.use(cookieParser());
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
+
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+
+app.use(express.json()); // allows us to parse incoming requests:req.body
+app.use(cookieParser()); // allows us to parse incoming cookies
+
 app.use("/api/auth", authRoutes);
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
@@ -29,5 +31,5 @@ if (process.env.NODE_ENV === "production") {
 
 app.listen(PORT, () => {
   connectDB();
-  console.log(`Server is running on port ${process.env.PORT}`);
+  console.log("Server is running on port: ", PORT);
 });
